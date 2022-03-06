7c25f8876d6f2dd5a5cd7eea32773ade66ce9039
Abinash <abinash261997@gmail.com>
2022-03-06 23:58:47 +0530

    footer section complete

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
         return false;
     });
 
+    //gallery pop up
     $(".project-area .button-group #btn1").trigger("click");
-
     $(".project-area .grid .test-popup-link").magnificPopup({
         type: "image",
         gallery: { enabled: true },
@@ -32,4 +32,20 @@ $(document).ready(function () {
             },
         },
     });
+
+    // sticky nav menu
+    let nav_offset_top = $(".header_area").height() + 50;
+    function navbarFixed() {
+        if ($(".header_area").length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $(".header_area .main-menu").addClass("navbar_fixed");
+                } else {
+                    $(".header_area .main-menu").removeClass("navbar_fixed");
+                }
+            });
+        }
+    }
+    navbarFixed();
 });
